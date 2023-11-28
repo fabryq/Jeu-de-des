635e9e3dfe99a2298e0997b4527d4b336dc61543
fabryq <120800630+fabryq@users.noreply.github.com>
2023-11-28 13:51:43 +0100

    first commit, html and css product

---
+++ script.js
@@ -0,0 +1,23 @@
+function test() {
+
+    2
+    
+      var a = 1;
+    
+    3
+    
+      var _b = 2;
+    
+    4
+    
+      console.log(a)
+    
+    5
+    
+    }
+    
+    6
+    
+    7
+    
+    test();
